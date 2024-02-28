50bead012550bef854efa27b1c7c4d4a2f5426c8
victoranyun <victoryun1811@gmail.com>
2024-02-28 18:38:19 -0500

    refactor: cleanup backend code

--- ZenJourney-backend/src/main/kotlin/com/backend/domain/entity/User.kt
+++ ZenJourney-backend/src/main/kotlin/com/backend/domain/entity/User.kt
@@ -1,19 +1,20 @@
 package com.backend.domain.entity
+
 import com.backend.application.response.UserResponse
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
 data class User(
-        @BsonId
-        val id: ObjectId,
-        val name: String,
-        val email: String,
-        val password: String
-){
+    @BsonId
+    val id: ObjectId,
+    val name: String,
+    val email: String,
+    val password: String
+) {
     fun toResponse() = UserResponse(
-            id = id.toString(),
-            name = name,
-            email = email,
-            password = password
+        id = id.toString(),
+        name = name,
+        email = email,
+        password = password
     )
 }

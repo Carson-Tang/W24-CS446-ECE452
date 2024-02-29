57b3678a296c5027e7f72bf68b53c45379da6d88
victoranyun <victoryun1811@gmail.com>
2024-02-28 21:20:08 -0500

    cleanup dependencies

--- ZenJourney/shared/build.gradle.kts
+++ ZenJourney/shared/build.gradle.kts
@@ -19,20 +19,5 @@ application {
 }
 
 dependencies {
-//    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-//    implementation("io.ktor:ktor-server-core-jvm")
-//    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-//    implementation("io.ktor:ktor-serialization-gson-jvm")
-//    implementation("io.ktor:ktor-server-call-logging-jvm")
-//    implementation("io.ktor:ktor-server-host-common-jvm")
-//    implementation("io.ktor:ktor-server-jetty-jvm")
-//    implementation("io.ktor:ktor-server-tomcat-jvm")
-//    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.11.0")
     implementation("org.mongodb:bson-kotlinx:4.11.0")
-//    //Koin Dependency Injection
-//    implementation("io.insert-koin:koin-ktor:3.5.3")
-//    implementation("io.insert-koin:koin-logger-slf4j:3.5.3")
-//    implementation("ch.qos.logback:logback-classic:$logback_version")
-//    testImplementation("io.ktor:ktor-server-tests-jvm")
-//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }

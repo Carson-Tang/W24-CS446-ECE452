3aef6803709f83a20708375b9a11f4dd2c013571
Alice Gao <alice.gao1@uwaterloo.ca>
2024-03-03 16:59:28 -0500

    remove unused imports

--- ZenJourney/app/src/main/java/ca/uwaterloo/cs/AuthPages.kt
+++ ZenJourney/app/src/main/java/ca/uwaterloo/cs/AuthPages.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme

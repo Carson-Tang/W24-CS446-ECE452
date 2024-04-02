5e5f8f83e1bb2614e445452fc1088661d260b715
nil-cipher <mmhomew@gmail.com>
2024-04-01 21:45:33 -0400

    Fix spacing in gradle.kts file

--- ZenJourney/app/build.gradle.kts
+++ ZenJourney/app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
   implementation("com.github.Abhimanyu14:compose-emoji-picker:1.0.0-alpha14")
   implementation("androidx.security:security-crypto:1.0.0")
 
-    testImplementation("junit:junit:4.13.2")
+  testImplementation("junit:junit:4.13.2")
   testImplementation("androidx.test.ext:junit:1.1.5")
   androidTestUtil("androidx.test:orchestrator:1.4.2")
   androidTestImplementation("androidx.test:runner:1.5.2")
